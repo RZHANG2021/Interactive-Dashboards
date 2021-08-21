94b4da72326a66dc64a8f61885dbeb0e72835a12
Rachel Zhang <mailrenxu@hotmail.com>
2021-08-22 00:34:27 +1000

    update on the js file

--- static/js/app.js
+++ static/js/app.js
@@ -3,101 +3,139 @@ function init() {
   //select the dropdown area
   var dropdown = d3.select("#selDataset");
 
-  //read through the json data file 
-  d3.json("samples.json").then((data) => {
-
-      //locate the ID data and set it to subjectID, append it to a drowpdown list
-      
-      d3.json("data/samples.json").then((data)=>{
-        // console.log(data);
-        subjectID = data[Object.keys(data)[0]] 
-        // console.log(subjectID);   
-        subjectID.forEach(function(id){
-          // console.log(id);
-          dropdown.append("option").text(id);})           
-         
+  //read through the json data file     
+  d3.json("data/samples.json").then((data)=>{
+    //locate the ID data and set it to subjectID, append it to a drowpdown list
+    subjectIDs = data[Object.keys(data)[0]] 
+    // console.log(subjectID);   
+    subjectIDs.forEach(function(id){
+    // console.log(id);
+      dropdown.append("option").text(id);              
     });
-      drawPlot(data.names[0]);
-      buildgauge(data.names[0]);
+    
+    // call Plot functions:
+    barPlot(data.names[0]);
+    bubblePlot(data.names[0]);
+    updateinfo(data.names[0]);
+    // call builtgauge function:
+    // buildgauge(data.names[0]);
   });
 }
 
 init();
 
 
+// set the function to create bar plots
+function barPlot(subjectid){
+//read through the json data file 
+  d3.json("samples.json").then((data) => {
+    // filter the sample object, locate the one that ID is the same as the subjectid
+    sampleData=data.samples.filter(i => i.id=== subjectid)[0];
+   console.log(sampleData)
+    //get the top 10 otu id
+    var top10 = sampleData.otu_ids.slice(0,10);     
+    var otutop10 = top10.map(t => "OTU "+ t);
 
-
-// function drawPlot(idNames){
-//   //read samples json first
-//   d3.json("samples.json").then((data) => {
-     
-//       //get the sample array match the idnames
-//       var sample = data.samples.filter(s => s.id.toString() === idNames)[0];
+    // get the top 10 otu values
+    var top10value= sampleData.sample_values.slice(0,10).reverse();
       
-//       //get the top 10 otu id
-//       var otuidstop10 = (sample.otu_ids.slice(0,10)).reverse();
+    // get the top 10 otu lavels 
+    var top10lables = sampleData.otu_labels.slice(0,10).reverse();
+      
+    // set up the trace of the bar chart
+    var bartrace = {
+          x:top10value,
+          y:otutop10,
+          text:top10lables,
+          orientation : 'h',
+          marker: {
+          color: '0069b3'},
+          type:"bar",
+          }
+      var data = [bartrace];
+
+      //create the layout variable to set plots layout
+      var barlayout = {
+          font:{family:"Courier New, monospace"},
+          title:"Top 10 Bacteria Cultures Found",
+          yaxis:{
+              tickmode:"linear",
+          },
+          margin:{
+              l:120,
+              r:40,
+              t:40,
+              b:40
+          }
+      }
+      Plotly.newPlot("bar",data,barlayout)
+
+       
+});
+}
     
-//       // change the format of out id for bar plot
-//       var otuforplot = otuidstop10.map(d => "OTU "+ d)
-      
-//       // find the sample_values for id
-//       var samplevalues = sample.sample_values.slice(0,10).reverse()
-      
-//       //find the otu lable 
-//       var otulabels = sample.otu_labels.slice(0,10)
-      
-//       // set up the trace of the bar chart
-//       var trace = {
-//           x:samplevalues,
-//           y:otuforplot,
-//           text:otulabels,
-//           orientation : 'h',
-//           marker: {
-//               color:'#005a99'},
-//           type:"bar",
-//           }
-//       var data = [trace];
 
-//       //create the layout variable to set plots layout
-//       var layout = {
-//           font:{family:"Arial Rounded MT Bold"},
-//           title:"Top 10 Bacteria Cultures Found",
-//           yaxis:{
-//               tickmode:"linear",
-//           },
-//           margin:{
-//               l:100,
-//               r:50,
-//               t:100,
-//               b:30
-//           }
-//       }
-//       Plotly.newPlot("bar",data,layout)
+// set the function to create bubble plots
+function bubblePlot(subjectid){
+  //read through the json data file 
+  d3.json("samples.json").then((data) => {
+    // filter the sample object, locate the one that ID is the same as the subjectid
+    sampleData=data.samples.filter(i => i.id=== subjectid)[0];
+    // console.log(sampleData)
       
-//       //Create the bubble chart
-//       var trace1 = {
-//           x : sample.otu_ids,
-//           y : sample.sample_values,
-//           mode: "markers",
-//           marker:{
-//               color:['rgb(253, 210, 191)','rgb(255, 107, 107)','rgb(182, 25, 25)','rgb(1, 36, 67)','rgb(70, 70, 96)','rgb(54, 139, 133)','rgb(180, 184, 151)','rgb(241, 233, 229)','rgb(0, 30, 108)','rgb(3, 83, 151)','rgb(80, 137, 198)','rgb(255, 170, 76)','rgb(236, 214, 98)','rgb(93, 130, 51)','rgb(40, 78, 120)','rgb(62, 33, 93)'],
-//               size:sample.sample_values
-//           },
-//           text:sample.otu_labels
-//       }
+    //Create the bubble chart
+    var bubbletrace = {
+            x : sampleData.otu_ids,
+            y : sampleData.sample_values,
+            mode: "markers",
+            marker:{
+                color:['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)','rgb(120,120,120)', 'rgb(120,120,120)', 'red', 'rgb(120,120,120)'],
+                size:sampleData.sample_values
+            },
+            text:sampleData.otu_labels
+          }
+  
+        //set the layout for the bubble plot
+        var bubblelayout ={
+            title:"Bacteria Cultures per sample",
+            font:{family:"Courier New, monospace"},
+            xaxis:{
+                title:"OTU ID",
+            },
+        }
+        var data2 = [bubbletrace];
+  
+        Plotly.newPlot("bubble",data2,bubblelayout)      
+  });
+  }
+// set the function to update infor box
+function updateinfo(subjectid){
+      d3.json("samples.json").then((data) => {
+          
+          //create the info box 
+          var metaData = data.metadata
+          console.log(metaData);
+          //select the result of infobox
+          var demoInfo = metaData.filter(i => i.id===subjectid)[0];
+          //select the frequency of wach
+          // var wfreq = results.wfreq
+      
+          //select the infor box area
+          var infoBox = d3.select("#sample-metadata")
+    
+          //empty the demographci info panel eath time before getting the new id
+          infoBox.html("");
+    
+          //grab the necessaty demographic data for the id and append the info to the infobox
+          Object.entries(demoInfo).forEach((d) => {infoBox.append("h5").text(d[0].toUpperCase() + ":"+ "\n"+ d[1] + "\n")});
+          
+  
+    });
+    }
 
-//       //set the layout for the bubble plot
-//       var layout1 ={
-//           font:{family:"Arial Rounded MT Bold"},
-//           xaxis:{
-//               title:"OTU ID",
-//           },
-//       }
-//       var data1 = [trace1];
 
-//       Plotly.newPlot("bubble",data1,layout1)
-//   });   
-// }
+
+
 
 
 // function buildgauge(idNames){
@@ -167,7 +205,7 @@ init();
 //       Plotly.newPlot("gauge", data2, layout2);
 // })
 // };  
-// function optionChanged(idNames) {
-//   drawPlot(idNames);
-//   buildgauge(idNames);
-// }
+function optionChanged(idNames) {
+  drawPlot(idNames);
+  buildgauge(idNames);
+}
